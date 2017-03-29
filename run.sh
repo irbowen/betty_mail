6e0efec3033236335851804579e6be5c8e54e424
Isaac Bowen <irbowen@umich.edu>
2017-03-29 00:25:24 -0400

    Adding pay link to email, better run.sh

--- run.sh
+++ run.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-echo "Cloning mailmerge"
-git clone https://github.com/mavcook/mailmerge.git
-cp filter_deter.py mailmerge/mailmerge/
+if [ -d mailmerge ]
+then
+	echo "Mailmerge is already here"
+else
+    echo "Cloning mailmerge"
+    git clone https://github.com/mavcook/mailmerge.git
+    cp filter_deter.py mailmerge/mailmerge/
+fi
 
 echo "Copy the starting db header over to the database"
 cp mailmerge_starting_database.csv mailmerge_database.csv
-
-
 
 shame_file='./shame.csv'
 if [ -f "$shame_file" ]
 then
 	echo "Using previous shame list..."
 else
-	echo "Avoiding errors..."
-	touch shame.csv
-	rm shame.csv
-
 	echo "Getting the shame list..."
 	wget https://chezbetty.eecs.umich.edu/shame.csv
 	dos2unix shame.csv
 fi
 
-echo "Adding @umich extensions..."
+echo "Building the mailmerge database from the shame list"
 ./util.py >> mailmerge_database.csv
 
 echo "Mail merge database setup! About to email..."
-python ./mailmerge/mailmerge/main.py --dry-run --no-limit --template-functions filter_deter
+python3 ./mailmerge/mailmerge/main.py --dry-run --no-limit --template-functions filter_deter
--- util.py
+++ util.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
-f = open('shame.csv')
-lines = f.readlines()[1:]
+with open('shame.csv') as f:
+    lines = f.readlines()[1:]
 
 for line in lines:
     line = line.strip('\n')
     words = line.split(',')
-    unique_name = words[0]
-    email = unique_name # + "@umich.edu"
     # Ignore those on the wall of shame less than a week
     if (int(words[3]) < 7):
         continue
-    print("%s,%s,%s,%s" % (email, words[1], words[2], words[3]))
+    print("%s,%s,%s,%s" % (words[0], words[1], words[2], words[3]))
 
